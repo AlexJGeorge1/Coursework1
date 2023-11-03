0e959e8235aabea8dd78dab4d2304ef351b9274a
AlexJGeorge1 <146655389+AlexJGeorge1@users.noreply.github.com>
2023-11-03 15:24:14 +0000

    Copied main function from vscode to github

--- StepsTask1.c
+++ StepsTask1.c
@@ -43,6 +43,45 @@ void tokeniseRecord(const char *input, const char *delimiter,
 
 // Complete the main function
 int main() {
-    
+  // Open the CSV file for reading
+  FILE * file = fopen("FitnessData_2023.csv", "r");
+  if (file == NULL) {
+    printf("FitnessData_2023.csv could not be opened\n");
+    return 1;
+  }
 
+  FITNESS_DATA data[1000];
+  // set the intial value for numRecords
+  int numRecords = 0;
+  char line[1000];
+
+  // Read and process each line in the CSV file
+  while (fgets(line, sizeof(line), file) != NULL) {
+    // create variables of the correct sizes 
+    char date[11];
+    char time[6];
+    char steps[11];
+
+    // Use the tokeniseRecord function to split the line into date, time, and steps
+    tokeniseRecord(line, ",", date, time, steps);
+
+    // Store the data in the FITNESS_DATA structure
+    strcpy(data[numRecords].date, date);
+    strcpy(data[numRecords].time, time);
+    data[numRecords].steps = atoi(steps);
+    // increase the numRecords value by 1 for each iteration of the for loop, the final value of numRecords will be the total number of records as each line will have been read
+    numRecords++;
+  }
+
+  // The numRecords variable now contains 
+  printf("There are: %d records in the file \n", numRecords);
+
+  // iterate through the rows using a for loop to print out the first 3 rows (i<3 (change number to print more or less rows))
+  for (int i = 0; i < 3 && i < numRecords; i++) {
+    printf("%s/%s/%d\n", data[i].date, data[i].time, data[i].steps);
+  }
+
+  //close the file 
+  fclose(file);
+  return 0;
 }
